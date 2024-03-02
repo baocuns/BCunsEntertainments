d2504af9e7f92bd55ae3a3e0f123f14057aac32f
Bao <baonv.23.student@fit.tdc.edu.vn>
2024-03-02 15:28:36 +0700

    categories: show and get

---
+++ src/main/java/com/cuns/bce/repositories/CategoriesComicRepository.java
@@ -0,0 +1,4 @@
+package com.cuns.bce.repositories;
+
+public interface CategoriesComicRepository extends org.springframework.data.jpa.repository.JpaRepository<com.cuns.bce.entities.CategoriesComic, java.lang.Long> ,org.springframework.data.jpa.repository.JpaSpecificationExecutor<com.cuns.bce.entities.CategoriesComic> {
+}
